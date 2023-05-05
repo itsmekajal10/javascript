8ed4da17e78c15b7360c4eef21884fd91484bc79
itsmekajal10 <kajalgadade0510@gmail.com>
2023-05-05 10:10:03 +0530

    variable assign
    variable assign

---
+++ hello.js
@@ -0,0 +1,22 @@
+
+
+
+
+console.log("kajal gadade");
+console.log("Addres:-pandharpur");
+console.log("Best Friend:-Rani");
+
+var firstName = "kajal";
+console.log(firstName);
+
+var age; // variable Declaration
+age = "22"; // variable Initialization
+console.log(age);
+
+var holder;//variable Declaration
+console.log(holder); //undefinend
+holder = "My Spec";
+console.log(holder);
+
+holder = "rani spec";
+console.log(holder);
---
+++ variable_assign.js
@@ -0,0 +1,19 @@
+
+
+var holder;//variable Declaration
+console.log(holder); //undefinend
+holder = "Kajal Dnyaneshwar Gadade";
+console.log(holder);
+
+var myLove;//variable Declaration
+myLove = "I love only JS";
+console.log(myLove);
+
+console.log("Kajal");
+console.log("Gadade");
+console.log("22");
+console.log("Pandharpur ,Dist-Solapur");
+console.log("413304");
+console.log("Female");
+console.log("No");
+
